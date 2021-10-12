ab17d05c88a9faea3f3bc8eec309efb43c8d0a05
suru268 <88202759+suru268@users.noreply.github.com>
2021-10-12 11:54:07 +0100

    Update hello_suru_fun.py

--- hello_suru_fun.py
+++ hello_suru_fun.py
@@ -4,17 +4,14 @@ print("Username is: " + username)
 
 print("Calling a Function!")
 
-def my_function_lname(fname):
-  print(fname + " First Name")
-  
-def my_function_fname(lname):
-  print(lname + " Last Name")
-
-my_function_fname("suru")
-my_function_fname("suru1")
-my_function_fname("suru2")
-
-
-my_function_lname("m")
-my_function_lname("m1")
-my_function_lname("m2")
+def my_function_fname(fname):
+   fname=input("Please Enter your First Name")
+   print(fname + "First Name")
+    
+def my_function_lname(lname):  
+   lname=input("Please Enter your Last Name")
+   print(lname + " Last Name") 
+    
+my_function_fname()
+my_function_lname()
+ 
